d91b50a773a57745634b11cac2411e873e150f73
Jean-Milost Reymond <jean_milost@hotmail.com>
2019-10-26 00:10:38 -0300

    - improved: cleaned up the zBaseLib project, changed class names to new standards, reformatted code, improved comments, ... (NOT TERMINATED) - cleaned: code and comments, renamed several projects names to conform them to new standards

--- Visual Mercutio/ZBaseLib/ZLVEdit.cpp
+++ Visual Mercutio/ZBaseLib/ZLVEdit.cpp
@@ -1,19 +1,24 @@
-// ZLVEdit.cpp : implementation of the CListEditView class
-//
+/****************************************************************************
+ * ==> PSS_ListViewEdit ----------------------------------------------------*
+ ****************************************************************************
+ * Description : Provides a list view edit                                  *
+ * Developer   : Processsoft                                                *
+ ****************************************************************************/
 
 #include "stdafx.h"
 #include "ZLVEdit.h"
 
 #ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
+    #define new DEBUG_NEW
+    #undef THIS_FILE
+    static char THIS_FILE[] = __FILE__;
 #endif
 
-/////////////////////////////////////////////////////////////////////////////
-// ZLVEdit
-BEGIN_MESSAGE_MAP(ZLVEdit, CEdit)
-    //{{AFX_MSG_MAP(ZLVEdit)
+//---------------------------------------------------------------------------
+// Message map
+//---------------------------------------------------------------------------
+BEGIN_MESSAGE_MAP(PSS_ListViewEdit, CEdit)
+    //{{AFX_MSG_MAP(PSS_ListViewEdit)
     ON_WM_KILLFOCUS()
     ON_WM_NCDESTROY()
     ON_WM_CHAR()
@@ -21,119 +26,135 @@ BEGIN_MESSAGE_MAP(ZLVEdit, CEdit)
     ON_WM_WINDOWPOSCHANGING()
     //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
-
-/////////////////////////////////////////////////////////////////////////////
-// ZLVEdit message handlers
-
-ZLVEdit::ZLVEdit(int iItem, int iSubItem, CString sInitText)
-: m_strInitText(sInitText), m_x(0), m_y(0)
-{    
-    m_iItem = iItem;
-    m_iSubItem = iSubItem;
-    m_bVK_ESCAPE = 0;
-}
-
-int ZLVEdit::OnCreate(LPCREATESTRUCT lpCreateStruct)
+//---------------------------------------------------------------------------
+// PSS_ListViewEdit
+//---------------------------------------------------------------------------
+PSS_ListViewEdit::PSS_ListViewEdit(int item, int subItem, const CString& initText) :
+    CEdit(),
+    m_InitText(initText),
+    m_Item(item),
+    m_SubItem(subItem),
+    m_X(0),
+    m_Y(0),
+    m_Escape(FALSE)
+{}
+//---------------------------------------------------------------------------
+PSS_ListViewEdit::~PSS_ListViewEdit()
+{}
+//---------------------------------------------------------------------------
+BOOL PSS_ListViewEdit::PreTranslateMessage(MSG* pMsg)
 {
-    if (CEdit::OnCreate(lpCreateStruct) == -1)        
-        return -1;    
-
-    CFont* font = GetParent()->GetFont();
-    SetFont(font);
-    SetWindowText(m_strInitText);
-    SetFocus();    
-    SetSel(0, 0);
-    return 0;
-}
-
-BOOL ZLVEdit::PreTranslateMessage(MSG* pMsg)
-{
-    if( pMsg->message == WM_KEYDOWN )    
-    {        
-        if(pMsg->wParam == VK_RETURN || pMsg->wParam == VK_DELETE || pMsg->wParam == VK_ESCAPE || GetKeyState( VK_CONTROL))
-        {            
+    if (pMsg->message == WM_KEYDOWN)
+        if (pMsg->wParam == VK_RETURN ||
+            pMsg->wParam == VK_DELETE ||
+            pMsg->wParam == VK_ESCAPE ||
+            ::GetKeyState(VK_CONTROL))
+        {
             ::TranslateMessage(pMsg);
             ::DispatchMessage(pMsg);
             return 1;
-        }    
-    }
+        }
+
     return CEdit::PreTranslateMessage(pMsg);
 }
+//---------------------------------------------------------------------------
+int PSS_ListViewEdit::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+    if (CEdit::OnCreate(lpCreateStruct) == -1)
+        return -1;
 
+    CFont* pFont = GetParent()->GetFont();
+    SetFont(pFont);
 
-void ZLVEdit::OnKillFocus(CWnd* pNewWnd)
-{    
+    SetWindowText(m_InitText);
+
+    SetFocus();
+    SetSel(0, 0);
+    return 0;
+}
+//---------------------------------------------------------------------------
+void PSS_ListViewEdit::OnKillFocus(CWnd* pNewWnd)
+{
     CEdit::OnKillFocus(pNewWnd);
 
-    CString str;    GetWindowText(str);
+    CString str;
+    GetWindowText(str);
 
-    // Send Notification to parent of ListView ctrl    
+    // send notification to list view parent
     LV_DISPINFO lvDispInfo;
-    lvDispInfo.hdr.hwndFrom = GetParent()->m_hWnd;
-    lvDispInfo.hdr.idFrom = GetDlgCtrlID();    
-    lvDispInfo.hdr.code = LVN_ENDLABELEDIT;
-    lvDispInfo.item.mask = LVIF_TEXT;    
-    lvDispInfo.item.iItem = m_iItem;
-    lvDispInfo.item.iSubItem = m_iSubItem;
-    lvDispInfo.item.pszText = m_bVK_ESCAPE ? NULL : LPTSTR((LPCTSTR)str);
+    lvDispInfo.hdr.hwndFrom    = GetParent()->m_hWnd;
+    lvDispInfo.hdr.idFrom      = GetDlgCtrlID();
+    lvDispInfo.hdr.code        = LVN_ENDLABELEDIT;
+    lvDispInfo.item.mask       = LVIF_TEXT;
+    lvDispInfo.item.iItem      = m_Item;
+    lvDispInfo.item.iSubItem   = m_SubItem;
+    lvDispInfo.item.pszText    = m_Escape ? NULL : LPTSTR(LPCTSTR(str));
     lvDispInfo.item.cchTextMax = str.GetLength();
-    GetParent()->GetParent()->SendMessage( WM_NOTIFY, GetParent()->GetDlgCtrlID(),(LPARAM)&lvDispInfo);
+    GetParent()->GetParent()->SendMessage(WM_NOTIFY, GetParent()->GetDlgCtrlID(), LPARAM(&lvDispInfo));
     DestroyWindow();
 }
-
-
-void ZLVEdit::OnNcDestroy()
+//---------------------------------------------------------------------------
+void PSS_ListViewEdit::OnNcDestroy()
 {
-    CEdit::OnNcDestroy();    
+    CEdit::OnNcDestroy();
     delete this;
 }
-
-
-void ZLVEdit::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
+//---------------------------------------------------------------------------
+void PSS_ListViewEdit::OnChar(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-    if( nChar == VK_ESCAPE || nChar == VK_RETURN)    
-    {        
-        if( nChar == VK_ESCAPE)
-            m_bVK_ESCAPE = 1;        
-        GetParent()->SetFocus();
-        return;    
+    switch (nChar)
+    {
+        case VK_ESCAPE:
+            m_Escape = TRUE;
+            GetParent()->SetFocus();
+            return;
+
+        case VK_RETURN:
+            GetParent()->SetFocus();
+            return;
     }
 
-    CEdit::OnChar(nChar, nRepCnt, nFlags);    
-    // Resize edit control if needed
-    // Get text extent    
-    CString str;    
-    GetWindowText( str );    
+    CEdit::OnChar(nChar, nRepCnt, nFlags);
+
+    // resize edit control if needed. Get text extent
+    CString str;
+    GetWindowText(str);
+
     CWindowDC dc(this);
-    CFont *pFont = GetParent()->GetFont();
-    CFont *pFontDC = dc.SelectObject(pFont);
-    CSize size = dc.GetTextExtent(str);    
+    CFont*    pFont   = GetParent()->GetFont();
+    CFont*    pFontDC = dc.SelectObject(pFont);
+    CSize     size    = dc.GetTextExtent(str);
     dc.SelectObject(pFontDC);
-    size.cx += 5; // add some extra buffer    
-    // Get client rect
-    CRect rect, rcParent;    
+
+    // add some extra buffer
+    size.cx += 5;
+
+    // get client rect
+    CRect rect, rcParent;
     GetClientRect(&rect);
     GetParent()->GetClientRect(&rcParent);
-    // Transform rect to parent coordinates    
+
+    // transform rect to parent coordinates
     ClientToScreen(&rect);
     GetParent()->ScreenToClient(&rect);
-    // Check whether control needs to be resized
-    // and whether there is space to grow    
-    if(size.cx > rect.Width())    
+
+    // check whether control needs to be resized, and whether there is space to grow
+    if (size.cx > rect.Width())
     {
-        if(size.cx + rect.left < rcParent.right)
-            rect.right = rect.left + size.cx;        
-        else            
+        if (size.cx + rect.left < rcParent.right)
+            rect.right = rect.left + size.cx;
+        else
             rect.right = rcParent.right;
-        MoveWindow(&rect);    
+
+        MoveWindow(&rect);
     }
 }
-
-
-void ZLVEdit::OnWindowPosChanging(WINDOWPOS FAR* lpwndpos) 
+//---------------------------------------------------------------------------
+void PSS_ListViewEdit::OnWindowPosChanging(WINDOWPOS FAR* lpwndpos)
 {
-    lpwndpos->x=m_x;
-    lpwndpos->y=m_y;
+    lpwndpos->x = m_X;
+    lpwndpos->y = m_Y;
+
     CEdit::OnWindowPosChanging(lpwndpos);
 }
-
+//---------------------------------------------------------------------------
--- Visual Mercutio/ZBaseLib/ZUFileLauncher.h
+++ Visual Mercutio/ZBaseLib/ZUFileLauncher.h
@@ -9,10 +9,10 @@
 #define AFX_ZUFILELAUNCHER_H__6E3CB810_2560_11D3_9828_00C04FB4D0D7__INCLUDED_
 
 #if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
+    #pragma once
+#endif
 
-//change the definition of AFX_EXT... to make it import
+// change the definition of AFX_EXT... to make it import
 #undef AFX_EXT_CLASS
 #undef AFX_EXT_API
 #undef AFX_EXT_DATA
@@ -20,57 +20,59 @@
 #define AFX_EXT_API AFX_API_IMPORT
 #define AFX_EXT_DATA AFX_DATA_IMPORT
 
-enum FileLauncherMode {    FLMedit,        // Opens an editor. If lpFile is not a document file, the function will fail. 
-                        FLMexplore,        // The function explores the folder specified by lpFile.  
-                        FLMopen,        // The function opens the file specified by the lpFile parameter. The file can be an executable file or a document file. It can also be a folder. 
-                        FLMprint,        // The function prints the document file specified by lpFile. If lpFile is not a document file, the function will fail. 
-                        FLMproperties };
+enum FileLauncherMode
+{
+    FLMedit,        // Opens an editor. If lpFile is not a document file, the function will fail. 
+    FLMexplore,        // The function explores the folder specified by lpFile.  
+    FLMopen,        // The function opens the file specified by the lpFile parameter. The file can be an executable file or a document file. It can also be a folder. 
+    FLMprint,        // The function prints the document file specified by lpFile. If lpFile is not a document file, the function will fail. 
+    FLMproperties
+};
 
 #ifdef _ZBASELIBEXPORT
-//put the values back to make AFX_EXT_CLASS export again
-#undef AFX_EXT_CLASS
-#undef AFX_EXT_API
-#undef AFX_EXT_DATA
-#define AFX_EXT_CLASS AFX_CLASS_EXPORT
-#define AFX_EXT_API AFX_API_EXPORT
-#define AFX_EXT_DATA AFX_DATA_EXPORT
+    // put the values back to make AFX_EXT_CLASS export again
+    #undef AFX_EXT_CLASS
+    #undef AFX_EXT_API
+    #undef AFX_EXT_DATA
+    #define AFX_EXT_CLASS AFX_CLASS_EXPORT
+    #define AFX_EXT_API AFX_API_EXPORT
+    #define AFX_EXT_DATA AFX_DATA_EXPORT
 #endif
 
 //#undef  AFX_DATA
 //#define AFX_DATA AFX_EXT_CLASS
 
-class AFX_EXT_CLASS ZUFileLauncher  
+class AFX_EXT_CLASS ZUFileLauncher
 {
 public:
 
-    ZUFileLauncher                ( const CString        Filename    = "",
-                                  FileLauncherMode    Mode        = FLMopen,
-                                  const CString        Parameters    = "",
-                                  const CString        Directory    = "" );
+    ZUFileLauncher(const CString        Filename = "",
+                   FileLauncherMode    Mode = FLMopen,
+                   const CString        Parameters = "",
+                   const CString        Directory = "");
 
-    virtual ~ZUFileLauncher        ();
+    virtual ~ZUFileLauncher();
 
-    BOOL    Initialize            ( const CString        Filename,
-                                  FileLauncherMode    Mode        = FLMopen,
-                                  const CString        Parameters    = "",
-                                  const CString        Directory    = "" );
+    BOOL Initialize(const CString        Filename,
+                    FileLauncherMode    Mode = FLMopen,
+                    const CString        Parameters = "",
+                    const CString        Directory = "");
 
-    BOOL    Launch                ( const CString        Filename,
-                                  FileLauncherMode    Mode        = FLMopen,
-                                  const CString        Parameters    = "",
-                                  const CString        Directory    = "" );
+    BOOL Launch(const CString        Filename,
+                FileLauncherMode    Mode = FLMopen,
+                const CString        Parameters = "",
+                const CString        Directory = "");
 
-    BOOL    Launch                ();
-    BOOL    Close                ();
-    BOOL    ActivateApplication    ();
-    BOOL    HasBeenLaunched        ();
+    BOOL Launch();
+    BOOL Close();
+    BOOL ActivateApplication();
+    BOOL HasBeenLaunched();
 
-    CString    GetErrorString        ();
+    CString GetErrorString();
 
-//    BOOL    IsError() const { return reinterpret_cast<int>(m_sei.hInstApp) <= 32; };
     BOOL IsError() const
     {
-        return ((int)m_ErrorCode) <= 32;//reinterpret_cast<int>( m_ErrorCode ) <= 32;
+        return ((int)m_ErrorCode) <= 32;
     };
 
     HANDLE GetApplicationHandle() const
@@ -84,17 +86,14 @@ public:
     };
 
 private:
+    CString          m_Filename;
+    FileLauncherMode m_Mode;
+    CString          m_Parameters;
+    CString          m_Directory;
+    SHELLEXECUTEINFO m_sei;
+    HANDLE           m_ErrorCode;
 
     CString GetModeVerbString();
-
-private:
-
-    CString                m_Filename;
-    FileLauncherMode    m_Mode;
-    CString                m_Parameters;
-    CString                m_Directory;
-    SHELLEXECUTEINFO    m_sei;
-    HANDLE                m_ErrorCode;
 };
 
-#endif // !defined(AFX_ZUFILELAUNCHER_H__6E3CB810_2560_11D3_9828_00C04FB4D0D7__INCLUDED_)
+#endif
